bf6a5e49cc0cb85e29a89e73be8467f820852e47
JONATHAN_SWIFT <juniorjonswift@gmail.com>
2024-01-02 19:26:48 +0000

    added if_overlap and get_distance_from_point functions which checks if a point overlaps over a given area and the distance from one point to another respectively

--- Engine/maths.py
+++ Engine/maths.py
@@ -20,3 +20,19 @@ def get_distance_to_mouse(x, y):
 
     c = math.sqrt((b*b) + (a*a))
     return c
+
+def get_distance_from_point(x1: object, y1: object, x2: object, y2: object) -> object:
+    b = x2 - x1
+    a = y2 - y1
+
+    c = math.sqrt((b*b) + (a*a))
+    return c
+
+def if_overlap(x1, y1, x2, y2, x2size, y2size):
+    if x1 >= x2 and x1 <= x2size:
+        if y1 >= y2 and y1 <= y2size:
+            return True
+        else:
+            return False
+    else:
+        return False
